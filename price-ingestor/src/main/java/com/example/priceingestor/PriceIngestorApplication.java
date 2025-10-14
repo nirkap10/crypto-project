a5fedcf60b45a5321e3ff75ed216b6a0d93547c9
Nir <kaplanir24@gmail.com>
2025-10-14 21:31:35 +0300

    created root pom file and added price ingester directory

---
+++ price-ingestor/src/main/java/com/example/priceingestor/PriceIngestorApplication.java
@@ -0,0 +1,13 @@
+package com.example.priceingestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceIngestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceIngestorApplication.class, args);
+	}
+
+}
---
+++ price-ingestor/src/test/java/com/example/priceingestor/PriceIngestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.priceingestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceIngestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
